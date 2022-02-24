ba23472c52051e6a4a59669bda9c0c11ef3618e4
Cager1 <filip.crnogorac98@hotmail.com>
2022-02-24 20:21:24 +0100

    Update on base url

--- dist/_nuxt/static/1645725664/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fregister","\u002Flogin","\u002Fforgot-password","\u002Freset-password"]})
--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   },
 
   axios: {
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'http://pzi022022.studenti.sumit.sum.ba/backend',
   },
 
   auth: {
